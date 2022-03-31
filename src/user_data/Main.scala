80bcd5807221118e6126993b725bdaaa67cbeb8c
mlapshin <mlapshin@uwaterloo.ca>
2022-03-31 00:02:48 -0400

    Stubbed the user communication server projects.

---
+++ src/user_data/Main.scala
@@ -0,0 +1,15 @@
+package user_data
+
+/**
+ * @file: Main.scala
+ * @description: The Main method of the User Encrypted Data Server program.
+ * @author: KeepId
+ * @date: March 30, 2022
+ */
+
+object UserData {
+  def main(args: Array[String]) = {
+    println("User Data Server")
+    println("Hello world!")
+  }
+}
---
+++ src/user_response/Main.scala
@@ -0,0 +1,15 @@
+package user_response
+
+/**
+ * @file: Main.scala
+ * @description: The Main method of the User Encrypted Data Server program.
+ * @author: KeepId
+ * @date: March 30, 2022
+ */
+
+object UserData {
+  def main(args: Array[String]) = {
+    println("User Response Server")
+    println("Hello world!")
+  }
+}
